0ac69e07bfc203c85f61d5bdb9b53971df1fabc2
Faker4564 <101092027+Faker4564@users.noreply.github.com>
2022-04-04 16:35:52 +0900

    ログイン・ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_kana_name, presence: true
+  validates :last_kana_name, presence: true
+  validates :birthday, presence: true
 end
