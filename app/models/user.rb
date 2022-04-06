bc02910025fbe8c2bd581ebdf36a8bbd28763590
Faker4564 <101092027+Faker4564@users.noreply.github.com>
2022-04-06 17:39:11 +0900

    バリデーションの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_kana_name, presence: true
-  validates :last_kana_name, presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: VALID_PASSWORD_REGEX
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/ } do
+    validates :first_kana_name
+    validates :last_kana_name
+  end
+
   validates :birthday, presence: true
 end
