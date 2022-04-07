ff639947bfa99f45a65ded790b5128c4705f7baa
Faker4564 <101092027+Faker4564@users.noreply.github.com>
2022-04-07 15:10:10 +0900

    ActiveStorageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :buy
+  has_one_attached :image
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :buys
          
   validates :nickname, presence: true
 
