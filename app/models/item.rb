7e81a2a52c2b9f26e68263e353d2059fe0846540
Faker4564 <101092027+Faker4564@users.noreply.github.com>
2022-04-10 09:41:52 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,24 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :buy
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :delivery_cahrge
+  belongs_to :delivery_date
+  belongs_to :prefecture
+  belongs_to :product_condition
+
+  with_options numericality: { other_than: 1 , message: "can't be blank" } do
+    validates :category_id
+    validates :delivery_charge_id
+    validates :delivery_date_id
+    validates :prefecture_id
+    validates :product_condition_id
+  end 
+
+  with_options format: { with: /\A[0-9]+\z/ } do
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+               presence: { message: "can't be blank" }
+  end
 end
