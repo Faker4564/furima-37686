671b23908e93c30b8f1ea69d6b2274b42862a15a
Faker4564 <101092027+Faker4564@users.noreply.github.com>
2022-04-10 09:41:31 +0900

    ActiveHashの導入

---
+++ app/models/delivery_date.rb
@@ -0,0 +1,9 @@
+class Delivery_date < ActiveHash::Base
+  self.data = [
+    {id: 1, name: '___'},{id: 2, name: '1〜2日で発送'},{id: 3, name: '2〜3日で発送'},
+    {id: 4, name: '4〜7日で発送'}
+  ]
+
+  include ActiveHash::Associations
+  has_many :items
+end
