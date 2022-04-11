317b28368035d6b4748072be532ed35818e1ff97
Faker4564 <101092027+Faker4564@users.noreply.github.com>
2022-04-11 15:06:06 +0900

    itemsテーブルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2022_04_07_055512) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
     t.string "product_name", null: false
     t.text "product_description", null: false
     t.integer "category_id", null: false
@@ -44,6 +45,7 @@ ActiveRecord::Schema.define(version: 2022_04_07_055512) do
     t.integer "price", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -65,4 +67,5 @@ ActiveRecord::Schema.define(version: 2022_04_07_055512) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "items", "users"
 end
