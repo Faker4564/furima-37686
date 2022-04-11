811e64d0fe1f45a32baa0d9e23bc89987ca090a1
Faker4564 <101092027+Faker4564@users.noreply.github.com>
2022-04-11 15:07:36 +0900

    ActiveHashの修正

--- app/models/delivery_date.rb
+++ app/models/delivery_date.rb
@@ -1,6 +1,6 @@
-class Delivery_date < ActiveHash::Base
+class DeliveryDate < ActiveHash::Base
   self.data = [
-    {id: 1, name: '___'},{id: 2, name: '1〜2日で発送'},{id: 3, name: '2〜3日で発送'},
+    {id: 1, name: '---'},{id: 2, name: '1〜2日で発送'},{id: 3, name: '2〜3日で発送'},
     {id: 4, name: '4〜7日で発送'}
   ]
 
