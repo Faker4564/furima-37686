dd0a3278e036cd99a4654a8345e40ef616a5746c
Faker4564 <101092027+Faker4564@users.noreply.github.com>
2022-04-11 15:08:47 +0900

    テストコードの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,21 +5,32 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-  belongs_to :delivery_cahrge
+  belongs_to :delivery_charge
   belongs_to :delivery_date
   belongs_to :prefecture
-  belongs_to :product_condition
+  belongs_to :product_condition 
 
-  with_options numericality: { other_than: 1 , message: "can't be blank" } do
+  with_options presence: true do
+    validates :image
+    validates :product_name
+    validates :product_description
     validates :category_id
     validates :delivery_charge_id
     validates :delivery_date_id
     validates :prefecture_id
     validates :product_condition_id
-  end 
-
-  with_options format: { with: /\A[0-9]+\z/ } do
-    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-               presence: { message: "can't be blank" }
   end
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :delivery_charge_id
+    validates :delivery_date_id
+    validates :prefecture_id
+    validates :product_condition_id
+  end
+
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+
 end
