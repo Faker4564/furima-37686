1f1d8790f9e956c60e17d3d30ee595a2cf20e6a4
Faker4564 <101092027+Faker4564@users.noreply.github.com>
2022-04-11 17:10:47 +0900

    指摘箇所の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @items = Item.all
   end
 
   def new
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_one :buy
+  # has_one :buy
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -29,8 +29,8 @@ class Item < ApplicationRecord
     validates :product_condition_id
   end
 
-  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  with_options presence: true do
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
 end
