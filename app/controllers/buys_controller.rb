6dbd86dcdf4a6b23ca448dabc2a1a5044cf0dde8
Faker4564 <101092027+Faker4564@users.noreply.github.com>
2022-04-17 17:45:46 +0900

    コントローラーの作成

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,10 +1,28 @@
 class BuysController < ApplicationController
+  before_action :find_item, only: [:index, :create]
 
   def index
-    @buy = Shipping.new
+    @buy_shipping = BuyShipping.new
   end
 
   def create
+    @buy_shipping = BuyShipping.new(buy_params)
+    if @buy_shipping.valid?
+       @buy_shipping.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
+private
+
+  def buy_params
+    params.require(:buy_shipping).permit(:postcode, :prefecture_id, :city, :block, :building, :tel).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def find_item
+    @item = Item.find(params[:item_id])
+  end
+  
 end
