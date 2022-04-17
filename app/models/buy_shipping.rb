5a05ccb30f736925b3090d77236e4876cfbd0f99
Faker4564 <101092027+Faker4564@users.noreply.github.com>
2022-04-17 17:45:34 +0900

    バリデーションの実装

--- app/models/buy_shipping.rb
+++ app/models/buy_shipping.rb
@@ -1,23 +1,24 @@
 class BuyShipping
-  include ActiveModel::Module
-  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city,
-                :block, :building, :tel, :buy_id, :token
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :block, :building, :tel
 
   with_options presence: true do
-    validates :prefecture_id
+    validates :user_id
+    validates :item_id
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :block
     validates :building
     validates :tel, numericality: { only_integer: true, }, length: { minimum: 10, maximum: 11 }
-    validates :token
+    #validates :token
   end
 
   VALID_POSTCODE_REGEX = /\A\d{3}[-]\d{4}\z/
   validates :postcode, presence: true, format: { with: VALID_POSTCODE_REGEX }
 
   def save
-    buy_shipping = BuyShipping.create(user_id: user_id, item_id: item_id)
-    Shipping.create(postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, building: building, tel: tel, buy_shipping_id: buy_shipping.id)
+    buy = Buy.create(user_id: user_id, item_id: item_id)
+    Shipping.create(postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, building: building, tel: tel, buy_id: buy.id)
   end
 
 end
